b7f212c36272f411c670b932dc5cfc89f180509c
LuisDani07 <tics270118@gmail.com>
2023-01-01 12:01:59 -0700

    esqueleto del proyecto

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 
-import {Navbar, Nav} from 'react-bootstrap'
+
 function App() {
   return (
-    <div className="container">
-         <Navbar bg="primary" variant="dark">
-              <Navbar.Brand href="#home">Home</Navbar.Brand>
-              <Nav className="mr-auto">
-                   <Nav.Link href="#home">Home</Nav.Link>
-                   <Nav.Link href="#features">Features</Nav.Link>
-                   <Nav.Link href="#pricing">Pricing</Nav.Link>
-              </Nav>
-         </Navbar>
+    <div >
+     app
     </div>
   );
 }
