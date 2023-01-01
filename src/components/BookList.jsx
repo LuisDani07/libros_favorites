a267ec489ef98a0a14f90c6a0ab69c6328b64bf3
LuisDani07 <tics270118@gmail.com>
2023-01-01 13:23:06 -0700

    trayendome los datos de la api url

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
-
+import {useState, useEffect} from 'react'
+import { API_URL } from '../API'
+import axios from 'axios'
 function BookList() {
+    const [books, setBooks]=useState([]);
+    useEffect(()=>{
+        axios.get(API_URL).then(res=>{
+            console.log(res.data);
+            setBooks(res.data)
+        }).catch(err=>console.log(err))
+    },[])
   return (
     <div className='book-list'>BookList
         <h1>hi</h1>
