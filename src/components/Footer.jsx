6ee07e86e4b0298a59db11dfadc859e8b5f3c715
LuisDani07 <tics270118@gmail.com>
2023-01-01 12:23:12 -0700

    componentes para la app de libros

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <p>Books App 2020&copy;</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbvar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbvar() {
+  return (
+    <div className='navbar'>
+        <div>
+            <h1>React Books App</h1>
+        </div>
+        <div>Your favorites</div>
+    </div>
+  )
+}
+
+export default Navbvar
